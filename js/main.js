598bc4b63dac511bf0e11e88ba19ce57a9649c29
Aosanders <sanderscross@gmail.com>
2018-02-28 14:09:25 -0800

    Added fade in effect on section 1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+   const x = document.querySelector(".section1-content h1");
+   const y = document.querySelector(".section1-content a");
+
+   x.classList.add("fadeInDown");
+   y.classList.add("fadeInUp");
+});
